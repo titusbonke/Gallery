381d85ac40e32e581046714d3761a4dd9db05872
titusbonke <titusbonkec@gmail.com>
2023-04-05 08:29:43 +0530

    completed read update delete on events and images

--- Images_Events.php
+++ Images_Events.php
@@ -93,14 +93,18 @@
                                 console.log(response);
                                 if (response == "OK") {
                                     $("#Result").html(`<div class="alert alert-success fade show" role="alert"> Event Created Successfully</div>`);
+                                    setTimeout(function () {
+                                        $("#Result").html('');
+                                        $('#CreateEventModal').modal('hide');
+                                        location.reload();
+                                    }, 5000);
                                 } else {
                                     $("#Result").html(`<div class="alert alert-danger fade show" role="alert"> ${response}</div>`);
+                                    setTimeout(function () {
+                                        $("#Result").html('');
+                                    }, 5000);
+
                                 }
-                                setTimeout(function () {
-                                    $("#Result").html('');
-                                    $('#CreateEventModal').modal('hide');
-                                    location.reload();
-                                }, 5000);
 
                             }
                         });
@@ -114,8 +118,7 @@
     </div>
 
     <!-- Modal -->
-    <div class="modal fade" id="EditEventModal" tabindex="-1" aria-labelledby="EditEventModalLabel"
-        aria-hidden="true">
+    <div class="modal fade" id="EditEventModal" tabindex="-1" aria-labelledby="EditEventModalLabel" aria-hidden="true">
         <div class="modal-dialog">
             <div class="modal-content">
                 <div class="modal-header">
@@ -133,14 +136,18 @@
                         <input type="datetime-local" name="EditEventDate" class="form-control" id="EditEventDate"
                             aria-describedby="EditEventDate" placeholder="">
                     </div>
-                    <div id="Result" class="m-3">
+                    <div class="UpdateDataId d-none"></div>
+                    <div id="EditResult" class="m-3">
 
                     </div>
 
 
                 </div>
                 <div class="modal-footer">
-                    <button type="button" class="btn btn-primary" id="EditEventBtn">Edit</button>
+                    <button type="button" class="btn btn-danger" id="DeleteEventBtn"
+                        onclick="DeleteEvent(this)">Delete</button>
+                    <button type="button" class="btn btn-primary" id="EditEventBtn"
+                        onclick="UpdateEvent(this)">Update</button>
                     <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
                 </div>
             </div>
@@ -154,19 +161,19 @@
     <div class="row text-center">
 
         <?php
-        //  print_r($EventRows)
+        //  print_r($EventRows);
         
         foreach ($EventRows as $Event) {
 
             echo '        <!-- Team item -->
         <div class="col-xl-3 col-sm-6 mb-5">
+        <div class="card   rounded shadow-sm px-3 py-5">
         <a href="Images_All.php?Id=' . $Event['g_id'] . '">
-            <div class="card   rounded shadow-sm px-3 py-5">
                 <img src="' . $Event['ImgUrl'] . '" alt="" width="100%"  class="img-fluid rounded-circle mb-3 img-thumbnail shadow-sm img-zoom-hover">
                 <h5 class="mb-0">' . $Event['g_title'] . '</h5><span class="small text-uppercase text-muted">' . date('M - Y', strtotime($Event['g_timestamp'])) . '</span>
-                <div class="p-2" ><i class="fas fa-edit" onclick="PreviewEvent(this)"></i></div>
+                </a>
+                <div class="p-2" ><i class="fas fa-edit" id="' . $Event['g_id'] . '" onclick="PreviewEvent(this)"></i></div>
             </div>
-            </a>
 
         </div>
         <!-- End -->
@@ -178,9 +185,92 @@
         ?>
         <script>
             function PreviewEvent(e) {
-                console.log(e);
-                $('#CreateEventModal').modal('show');
+                // console.log(e);
+                $('#EditEventModal').modal('show');
+                $(".UpdateDataId").html(e.id);
+                var id = e.id;
+                $.ajax({
+                    url: 'Functions.php',
+                    type: 'POST',
+                    data: { EventId: id, Function: "ReadEvent" },
+                    success: function (Data) {
+                        Data = JSON.parse(Data);
+                        console.log(Data);
+                        if (Data.Response == "OK") {
+                            $("#EditEventTitle").val(Data.g_title);
+                            $("#EditEventDate").val(Data.g_timestamp);
+                        } else {
+                            $("#EditResult").html(`<div class="alert alert-danger fade show" role="alert"> ${Data.Response}</div>`);
+                            setTimeout(function () {
+                                $("#EditResult").html('');
+                            }, 5000);
+
+                        }
+                    }
+                });
+
             }
+
+            function UpdateEvent(e) {
+                var id = $(".UpdateDataId").html();
+                var EditEventTitle=$("#EditEventTitle").val();
+                var EditEventDate=$("#EditEventDate").val();
+
+                console.log(id+EditEventTitle);
+                $.ajax({
+                    url: 'Functions.php',
+                    type: 'POST',
+                    data: { EventId: id,EventTitle:EditEventTitle,EventDate:EditEventDate, Function: "UpdateEvent" },
+                    success: function (response) {
+                        console.log(response);
+                        if (response == "OK") {
+                            $("#EditResult").html(`<div class="alert alert-success fade show" role="alert"> Event Updated Successfully</div>`);
+                            setTimeout(function () {
+                                $("#EditResult").html('');
+                                $('#EditEventModal').modal('hide');
+                                location.reload();
+                            }, 5000);
+                        } else {
+                            $("#EditResult").html(`<div class="alert alert-danger fade show" role="alert"> ${response}</div>`);
+                            setTimeout(function () {
+                                $("#EditResult").html('');
+                            }, 5000);
+
+                        }
+
+                    }
+                });
+            }
+
+            function DeleteEvent(e) {
+                var id = $(".UpdateDataId").html();
+
+                console.log(id);
+                $.ajax({
+                    url: 'Functions.php',
+                    type: 'POST',
+                    data: { EventId: id, Function: "DeleteEvent" },
+                    success: function (response) {
+                        console.log(response);
+                        if (response == "OK") {
+                            $("#EditResult").html(`<div class="alert alert-danger fade show" role="alert"> Event Deleted Successfully</div>`);
+                            setTimeout(function () {
+                                $("#EditResult").html('');
+                                $('#EditEventModal').modal('hide');
+                                location.reload();
+                            }, 5000);
+                        } else {
+                            $("#EditResult").html(`<div class="alert alert-danger fade show" role="alert"> ${response}</div>`);
+                            setTimeout(function () {
+                                $("#EditResult").html('');
+                            }, 5000);
+
+                        }
+
+                    }
+                });
+            }
+
         </script>
 
         <?php
